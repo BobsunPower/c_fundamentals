9c575a29ba4f86c89eb46889efb228079c18af82
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-07 18:24:30 +0300

    Create x10.MultiplicationTable2.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops - Lab/x10.MultiplicationTable2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace x10.MultiplicationTable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); int m = int.Parse(Console.ReadLine());
+            if (m > 10) {Console.WriteLine($"{n} X {m} = {n * m}"); return;}
+            for (int i = m; i < 11; i++) {Console.WriteLine($"{n} X {i} = {n * i}");}
+        }
+    }
+}
