1d1de8354ead7d0448b466553070941ad6ebb0d7
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-07 18:28:10 +0300

    Update x01.StudentInformation.cs

--- 01.Basic Syntax, Conditional Statements and Loops - Lab/x01.StudentInformation.cs
+++ 01.Basic Syntax, Conditional Statements and Loops - Lab/x01.StudentInformation.cs
@@ -6,10 +6,10 @@ namespace x01.StudentInformation
     {
         static void Main(string[] args)
         {
-            string student = Console.ReadLine();
+            string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
             double grade = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Name: {student}, Age: {age}, Grade: {grade:f2}");
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
         }
     }
 }
