d2071e8f86195cfba0cd514a3df474d64acc660d
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-08 14:30:17 +0300

    Create x06.Login.cs

---
+++ 02.Basic Syntax, Conditional Statements and Loops - Exercise/x06.Login.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace x06.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string usr = Console.ReadLine(); string pwd = ""; int ctr = 0;
+            for (int i = usr.Length - 1; i > -1; i--) {pwd += usr[i];}
+            while (true) {ctr++; string input = Console.ReadLine();
+                if (input == pwd) {Console.WriteLine($"User {usr} logged in."); break;}
+                if (ctr == 4) { Console.WriteLine($"User {usr} blocked!"); break; }
+                Console.WriteLine("Incorrect password. Try again.");}            
+        }
+    }
+}
