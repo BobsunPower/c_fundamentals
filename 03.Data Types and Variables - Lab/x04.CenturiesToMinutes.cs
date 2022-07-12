d5e0b21386bd2233a5216b28bab2b41b891cafbe
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-12 13:05:52 +0300

    Create x04.CenturiesToMinutes.cs

---
+++ 03.Data Types and Variables - Lab/x04.CenturiesToMinutes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace x04.CenturiesToMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine()); int y = c * 100; int d = (int)(y * 365.2422); int h = d * 24; int m = h * 60;
+            Console.WriteLine($"{c} centuries = {y} years = {d} days = {h} hours = {m} minutes");
+        }
+    }
+}
