0b5114a6bab03ea547ec59b68510e2307b7e66ea
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-14 08:46:42 +0300

    Create x05.PrintPartOfASCIITable.cs

---
+++ 04.Data Types and Variables - Exercise/x05.PrintPartOfASCIITable.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace x05.PrintPartOfASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine()); int end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++) {Console.Write($"{(char)i} ");}
+        }
+    }
+}
