dba0d359ee5717377664a653627c58961d3f9229
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-15 13:57:50 +0300

    Create x01.DayOfWeekcs.cs

---
+++ 05.Arrays - Lab/x01.DayOfWeekcs.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace x01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int day = int.Parse(Console.ReadLine());
+            if (day > 0 && day <= 7) {Console.WriteLine(days[day - 1]);}
+            else {Console.WriteLine("Invalid day!");}
+        }
+    }
+}
