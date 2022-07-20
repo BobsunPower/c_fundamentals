624dedc1f752995a7a0efdd6f0321829fda57178
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-20 09:37:18 +0300

    Create x07.MagicSum.cs

---
+++ 06.Arrays - Exercise/x07.MagicSum.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+namespace x07.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine() .Split(). Select(int.Parse). ToArray();
+            int magic = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nums.Length; i++)
+            {for (int j = i + 1; j < nums.Length; j++)
+                {if (nums[i] + nums[j] == magic) {Console.Write($"{nums[i]} {nums[j]}\n");}}}
+        }
+    }
+}
