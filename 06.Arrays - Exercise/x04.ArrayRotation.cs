ff8fd3d60d282e0c4b795b0c7c67d861246ab469
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-20 09:19:02 +0300

    Update x04.ArrayRotation.cs

--- 06.Arrays - Exercise/x04.ArrayRotation.cs
+++ 06.Arrays - Exercise/x04.ArrayRotation.cs
@@ -7,7 +7,7 @@ namespace x04.ArrayRotation
     {
         static void Main(string[] args)
         {
-            int[] nums = Console.ReadLine() .Split(' ') .Select(int.Parse) .ToArray();
+            int[] nums = Console.ReadLine() .Split() .Select(int.Parse) .ToArray();
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++) {int fst = nums[0];
                 for (int j = 1; j < nums.Length; j++) {nums[j - 1] = nums[j];}
