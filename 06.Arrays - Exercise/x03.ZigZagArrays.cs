b462b07cebd3b7b4f6af49cdcd4da15afdbaa0a3
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-20 09:20:02 +0300

    Update x03.ZigZagArrays.cs

--- 06.Arrays - Exercise/x03.ZigZagArrays.cs
+++ 06.Arrays - Exercise/x03.ZigZagArrays.cs
@@ -9,7 +9,7 @@ namespace x03.ZigZagArrays
         {
             int n = int.Parse(Console.ReadLine()); int[] fst = new int[n]; int[] snd = new int[n];
             for (int i = 0; i < n; i++)
-            {int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            {int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
                 if (i % 2 == 0) {snd[i] = line[0]; fst[i] = line[1];}
                 else {snd[i] = line[1];fst[i] = line[0];}}
             Console.WriteLine(string.Join(' ', snd));
