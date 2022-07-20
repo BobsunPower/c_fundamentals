f059ee2b4ff5edf5703047a4075ead04aae93742
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-20 09:31:54 +0300

    Create x07.MaxSequenceOfEqualElements.cs

---
+++ 06.Arrays - Exercise/x07.MaxSequenceOfEqualElements.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+namespace x07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine() .Split() .Select(int.Parse) .ToArray();
+            int maxSize = 0; int max = 0;
+            for (int i = 0; i < nums.Length; i++) {int size = 1; int n = nums[i];
+                for (int j = i + 1; j < nums.Length; j++) {
+                    if (nums[i] == nums[j]) {size++;}
+                    else {break;}}
+                if (size > maxSize) {maxSize = size; max = n;}}
+            for (int i = 0; i < maxSize; i++) {Console.Write($"{max} ");}
+        }
+    }
+}
