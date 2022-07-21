9ae23a9bcf8a9d9df77bf803239b929f402228ef
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-21 09:57:09 +0300

    Create x02.Grades.cs

---
+++ 07.Methods - Lab/x02.Grades.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace x02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            PrintInWords(grade);
+        }
+
+        static void PrintInWords(double g)
+        {
+            string r = "";
+            if (g < 3) {r = "Fail";}
+            else if (g < 3.5) {r = "Poor";}
+            else if (g < 4.5) {r = "Good";}
+            else if (g < 5.5) {r = "Very good";}
+            else {r = "Excellent";}
+            Console.WriteLine(r);
+        }
+    }
+}
