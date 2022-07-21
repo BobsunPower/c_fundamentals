59a02bdc8b28f10f4c67636c3b46df3f4cd19735
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-21 10:12:12 +0300

    Create x05.Orders.cs

---
+++ 07.Methods - Lab/x05.Orders.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace x05.Orders
+{
+    internal class Program
+    {
+        private static void OrderPrice(string name, int num)
+        {double res = 0;
+            switch (name) {
+                case "coffee": res = num * 1.50; break;
+                case "water": res = num * 1.00; break;
+                case "snacks": res = num * 2.00; break;
+                case "coke": res = num * 1.40; break;}
+            Console.WriteLine($"{res:f2}");}
+        static void Main(string[] args)
+        {
+            string cmd = Console.ReadLine(); int qty = int.Parse(Console.ReadLine());
+            OrderPrice(cmd, qty);
+        }
+
+        
+    }
+}
