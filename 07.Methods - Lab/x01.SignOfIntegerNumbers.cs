d9c0dd49f8b616871995100e1a28d67dffdcbdfd
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-21 09:54:01 +0300

    Create x01.SignOfIntegerNumbers.cs

---
+++ 07.Methods - Lab/x01.SignOfIntegerNumbers.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace x01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingOfNumber();
+        }
+
+        static void SingOfNumber()
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0) {Console.WriteLine($"The number {n} is zero. ");}
+            else if (n > 0) {Console.WriteLine($"The number {n} is positive.");}
+            else {Console.WriteLine($"The number {n} is negative.");}
+        }
+    }
+}
