a920f23b4a3db2cc75a3bdea66db2c688a51a339
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-22 09:58:14 +0300

    Create x11.MathOperations.cs

---
+++ 07.Methods - Lab/x11.MathOperations.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace x11.MathOperations
+{
+    internal class Program
+    {
+        static double Calculate(int x, string s, int y)
+        {
+            double result = 0;
+            switch (s) {
+                case "+": result = x + y; break;
+                case "-": result = x - y; break;
+                case "*": result = x * y; break;
+                case "/": result = x / y; break;}
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine()); string sign = Console.ReadLine(); int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(a, sign, b));
+        }        
+    }
+}
