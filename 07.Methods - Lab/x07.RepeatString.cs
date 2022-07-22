3a137ea2d5cfc4f502a51f58ef07025b51495ff8
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-22 09:21:14 +0300

    Create x07.RepeatString.cs

---
+++ 07.Methods - Lab/x07.RepeatString.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+
+namespace x07.RepeatString
+{
+    internal class Program
+    {
+        private static string RepeatString(string str, int rep)
+        {
+            StringBuilder txt = new StringBuilder();
+            for (int i = 0; i < rep; i++) {txt.Append(str);}
+            return txt.ToString();
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); int r = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(s, r));
+        }
+
+        
+    }
+}
