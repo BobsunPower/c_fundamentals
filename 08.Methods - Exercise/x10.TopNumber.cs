6dbc009bee2b47cc8453f84606bbae64e5ea4163
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-25 20:37:39 +0300

    Create x10.TopNumber.cs

---
+++ 08.Methods - Exercise/x10.TopNumber.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace x10.TopNumber
+{
+    internal class Program
+    {
+        static bool Divisability(int number)
+        {
+            int sum = 0;
+            while (number != 0) {int dgt = number % 10; sum += dgt; number /= 10;}
+            if (sum % 8 == 0) {return true;}
+            return false;
+        }
+
+        static bool OddDigit(int n)
+        {
+            while (n != 0) {int dgt = n % 10;
+                if (dgt % 2 == 1) {return true;}
+                n /= 10;}
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {if (Divisability(i) && OddDigit(i)) {Console.WriteLine(i);}}
+        }        
+    }
+}
