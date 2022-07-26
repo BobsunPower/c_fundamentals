2277eeaae86d2e7a0d52fcdca38d92df7a1ab994
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-26 16:24:14 +0300

    Create x04.ListOfProducts.cs

---
+++ 09.Lists - Lab/x04.ListOfProducts.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace x04.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++) {string pdt = Console.ReadLine(); products.Add(pdt);}
+            products.Sort();
+            for (int i = 0; i < products.Count; i++) {Console.WriteLine($"{i + 1}.{products[i]}");}
+        }
+    }
+}
