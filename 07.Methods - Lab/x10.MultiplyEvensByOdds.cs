09e5852e44a84c789ed8af5895465fa93bf5d63a
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-28 10:55:13 +0300

    Update x10.MultiplyEvensByOdds.cs

--- 07.Methods - Lab/x10.MultiplyEvensByOdds.cs
+++ 07.Methods - Lab/x10.MultiplyEvensByOdds.cs
@@ -4,34 +4,30 @@ namespace x10.MultiplyEvensByOdds
 {
     internal class Program
     {
-        static int GetSumOfEvenDigits(int n)
-        {
-            int sum = 0;
-            while (n != 0)
-            {int dgt = n % 10;
-                if (dgt % 2 == 0) { sum += dgt;}
-                n /= 10;}
-            return sum;
-        }
-
-        static int GetSumOfOddDigits(int n)
-        {
-            int sum = 0;
-            while (n != 0)
-            {int dgt = n % 10;
-                if (dgt % 2 != 0) { sum += dgt;}
-                n /= 10;}
-            return sum;
-        }
-
-        static int GetMultipleOfEvenAndOdds(int even, int odd)
-        {
-            return even * odd;
-        }
         static void Main(string[] args)
         {
-            int num = Math.Abs(int.Parse(Console.ReadLine())); int sumEven = GetSumOfEvenDigits(num); int sumOdd = GetSumOfOddDigits(num);
-            Console.WriteLine(GetMultipleOfEvenAndOdds(sumEven, sumOdd));
+            int num = Math.Abs(int.Parse(Console.ReadLine())); int sumEven = SumEvenDigits(num); int sumOdd = SumOddDigits(num);
+            Console.WriteLine(MultiplyEvenOdds(sumEven, sumOdd));
+        }
+        static int SumEvenDigits(int n)
+        {
+            int sum = 0;
+            while (n != 0) {int dgt = n % 10;
+                if (dgt % 2 == 0) {sum += dgt;}
+                n /= 10;}
+            return sum;
+        }
+        static int SumOddDigits(int n)
+        {
+            int sum = 0;
+            while (n != 0) {int dgt = n % 10;
+                if (dgt % 2 != 0) {sum += dgt;}
+                n /= 10;}
+            return sum;
+        }
+        static int MultiplyEvenOdds(int even, int odd)
+        {
+            return even * odd;
         }        
     }
 }
