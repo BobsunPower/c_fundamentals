7823044c010a4fae59e757f03d55df13e622f62e
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-28 10:48:19 +0300

    Update x08.MathPower.cs

--- 07.Methods - Lab/x08.MathPower.cs
+++ 07.Methods - Lab/x08.MathPower.cs
@@ -4,16 +4,16 @@ namespace x08.MathPower
 {
     internal class Program
     {
-        static double RaiseToPower(double n, int p)
-        {
-            double res = 1;
-            for (int i = 0; i < p; i++) {res *= n;}
-            return res;
-        }
         static void Main(string[] args)
         {
             double num = double.Parse(Console.ReadLine()); int pwr = int.Parse(Console.ReadLine());
             Console.WriteLine(RaiseToPower(num, pwr));
+        }
+        static double RaiseToPower(double n, int p)
+        {
+            double res = 1;
+            for (int i = 0; i < p; i++) { res *= n; }
+            return res;
         }        
     }
 }
