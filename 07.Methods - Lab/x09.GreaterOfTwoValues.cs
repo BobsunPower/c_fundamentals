c6145b7bf070a18bed8f92dcdebec5f08bfdd63a
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-28 10:51:57 +0300

    Update x09.GreaterOfTwoValues.cs

--- 07.Methods - Lab/x09.GreaterOfTwoValues.cs
+++ 07.Methods - Lab/x09.GreaterOfTwoValues.cs
@@ -4,34 +4,30 @@ namespace x09.GreaterOfTwoValues
 {
     internal class Program
     {
-        static int GetMax(int a, int b)
-        {
-            return Math.Max(a, b);
-        }
-
-        static char GetMax(char a, char b)
-        {
-            return (char)Math.Max(a, b);
-        }
-
-        static string GetMax(string a, string b)
-        {
-            int comparison = a.CompareTo(b);
-            if (comparison > 0) { return a; }
-            else { return b; }
-        }
         static void Main(string[] args)
         {
             string data = Console.ReadLine();
             switch (data) {
                 case "int": int fstNum = int.Parse(Console.ReadLine()); int sndNum = int.Parse(Console.ReadLine());
-                    Console.WriteLine(GetMax(fstNum, sndNum)); break;
+                    Console.WriteLine(Max(fstNum, sndNum)); break;
                 case "char": char fstChr = char.Parse(Console.ReadLine()); char sndChr = char.Parse(Console.ReadLine());
-                    Console.WriteLine(GetMax(fstChr, sndChr)); break;
-                case "string":
-                    string fstStr = Console.ReadLine(); string sndStr = Console.ReadLine();
-                    Console.WriteLine(GetMax(fstStr, sndStr)); break;
-            }
-        }      
+                    Console.WriteLine(Max(fstChr, sndChr)); break;
+                case "string": string fstStr = Console.ReadLine(); string sndStr = Console.ReadLine();
+                    Console.WriteLine(Max(fstStr, sndStr)); break;}
+        }
+        static int Max(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+        static char Max(char a, char b)
+        {
+            return (char)Math.Max(a, b);
+        }
+        static string Max(string a, string b)
+        {
+            int cmp = a.CompareTo(b);
+            if (cmp > 0) {return a;}
+            else {return b;}
+        }        
     }
 }
