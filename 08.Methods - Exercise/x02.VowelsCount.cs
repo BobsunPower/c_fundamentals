46a8fe17efe9cbc5bf0c1344a3f7300f2c806100
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-29 07:53:42 +0300

    Update x02.VowelsCount.cs

--- 08.Methods - Exercise/x02.VowelsCount.cs
+++ 08.Methods - Exercise/x02.VowelsCount.cs
@@ -5,14 +5,15 @@ namespace x02.VowelsCount
 {
     internal class Program
     {
-        static int VowelCounter(string str)
-        {
-            string txt = str.ToLower(); int cnt = 0; char[] vow = new char[] { 'a', 'o', 'e', 'u', 'i' };
-            foreach (char ch in txt) { if (vow.Contains(ch)) { cnt++; }} return cnt;
-        }
         static void Main(string[] args)
         {
             string str = Console.ReadLine(); Console.WriteLine(VowelCounter(str));
+        }
+        static int VowelCounter(string str)
+        {
+            string txt = str.ToLower(); int cnt = 0; char[] vow = new char[] { 'a', 'o', 'e', 'u', 'i' };
+            foreach (char ch in txt) { if (vow.Contains(ch)) { cnt++; } }
+            return cnt;
         }        
     }
 }
