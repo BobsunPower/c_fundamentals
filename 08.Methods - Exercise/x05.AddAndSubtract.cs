a92232f9aaece2a74cc9653a0a1fce880b72c72e
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-29 08:04:50 +0300

    Update x05.AddAndSubtract.cs

--- 08.Methods - Exercise/x05.AddAndSubtract.cs
+++ 08.Methods - Exercise/x05.AddAndSubtract.cs
@@ -3,21 +3,20 @@ using System;
 namespace x05.AddAndSubtract
 {
     internal class Program
-    {        
-        static int Sum(int fst, int snd)
-        {
-            return fst + snd;
-        }
-
-        static int Subtract(int fst, int snd)
-        {
-            return fst - snd;
-        }
+    {
         static void Main(string[] args)
         {
             int fst = int.Parse(Console.ReadLine()); int snd = int.Parse(Console.ReadLine()); int trd = int.Parse(Console.ReadLine());
             int res = Subtract(Sum(fst, snd), trd);
             Console.WriteLine(res);
         }
+        static int Sum(int fst, int snd)
+        {
+            return fst + snd;
+        }
+        static int Subtract(int fst, int snd)
+        {
+            return fst - snd;
+        }        
     }
 }
