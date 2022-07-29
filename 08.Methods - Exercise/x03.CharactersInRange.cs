7641307e7c22c115e273be7f508d54ef55d5ae87
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-29 07:57:47 +0300

    Update x03.CharactersInRange.cs

--- 08.Methods - Exercise/x03.CharactersInRange.cs
+++ 08.Methods - Exercise/x03.CharactersInRange.cs
@@ -4,17 +4,14 @@ namespace x03.CharactersInRange
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine()); char b = char.Parse(Console.ReadLine()); CharactersInRange(a, b);
+        }
         static void CharactersInRange(char a, char b)
         {
             int fst = Math.Min(a, b); int snd = Math.Max(a, b);
             for (int i = ++fst; i < snd; i++) {Console.Write((char)i + " ");}
         }
-        static void Main(string[] args)
-        {
-            char a = char.Parse(Console.ReadLine()); char b = char.Parse(Console.ReadLine());
-            CharactersInRange(a, b);
-        }
-
-        
     }
 }
