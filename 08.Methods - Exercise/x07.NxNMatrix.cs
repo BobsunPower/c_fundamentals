e1e3737a52cd4d4e6ae8c920e8edd03f0ff7961d
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-29 08:09:45 +0300

    Update x07.NxNMatrix.cs

--- 08.Methods - Exercise/x07.NxNMatrix.cs
+++ 08.Methods - Exercise/x07.NxNMatrix.cs
@@ -4,16 +4,16 @@ namespace x07.NxNMatrix
 {
     internal class Program
     {
-        static void PrintMatrix(int n)
-        {
-            for (int i = 0; i < n; i++)
-            {for (int j = 0; j < n; j++) {Console.Write(n + " ");}
-                Console.WriteLine();}
-        }
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
             PrintMatrix(n);
         }
+        static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {Console.Write(n + " ");}
+                Console.WriteLine();}
+        }        
     }
 }
