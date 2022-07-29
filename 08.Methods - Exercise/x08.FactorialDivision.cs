f7b00e3fc5b1699779e180e3f83078f73c19a53b
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-07-29 08:12:56 +0300

    Update x08.FactorialDivision.cs

--- 08.Methods - Exercise/x08.FactorialDivision.cs
+++ 08.Methods - Exercise/x08.FactorialDivision.cs
@@ -4,16 +4,16 @@ namespace x08.FactorialDivision
 {
     internal class Program
     {
-        static double Factorial(int n)
-        {
-            double res = 1;
-            for (int i = 1; i <= n; i++) {res *= i;}
-            return res;
-        }
         static void Main(string[] args)
         {
             int fst = int.Parse(Console.ReadLine()); int snd = int.Parse(Console.ReadLine());
             Console.WriteLine($"{Factorial(fst) / Factorial(snd):f2}");
         }
+        static double Factorial(int n)
+        {
+            double res = 1;
+            for (int i = 1; i <= n; i++) {res *= i;}
+            return res;
+        }        
     }
 }
